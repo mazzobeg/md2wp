2a4a46d5783118ebc811086f836fbc23045943ab
mazzobeg <mazzobel.gvn@gmail.com>
2022-12-10 23:56:49 +0100

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='md2wp',
+    version='0.0.1',
+    install_requires=[
+        'beautifulsoup4',
+        'markdown2',
+        'requests',
+        'tabulate'
+    ],
+    entry_points={
+        'console_scripts': [
+            'md2wp = md2wp.__main__:main',
+        ]
+    }
+)
